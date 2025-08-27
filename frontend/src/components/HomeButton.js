8c07b07a7e940d076ed9e612b083b0c99233d9f7
jyd0328 <jydjyd0328@gmail.com>
2025-08-27 14:41:40 +0900

    fix: home button icon

--- frontend/src/components/HomeButton.js
+++ frontend/src/components/HomeButton.js
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import styles from "./HomeButton.module.scss";
+import { HiOutlineHome } from "react-icons/hi2";
 
 // 홈 버튼. 기본적으로 40x40에 아이콘 설정되어있음.
 // 원하는 링크를 props로 받아서 작동.
@@ -11,7 +12,7 @@ export const HomeButton = ({ to }) => {
 
   return (
     <button className={styles.homeButton} onClick={handleClick} type="button">
-      <img src="/icons/Home_Icon.png" alt="home" className={styles.homeIcon} />
+      <HiOutlineHome size={24} />
     </button>
   );
 };
--- frontend/src/components/PageTitle.js
+++ frontend/src/components/PageTitle.js
@@ -1,5 +1,8 @@
 import styles from "./PageTitle.module.scss";
 
+//각 탭 별 제목을 띄우는 컴포넌트
+//탭의 제목, 양 옆 아이콘, 아이콘의 크기를 지정해서 사용한다.
+
 export const PageTitle = ({ title, Icon, size = 31 }) => {
   return (
     <div className={styles.wrap}>
