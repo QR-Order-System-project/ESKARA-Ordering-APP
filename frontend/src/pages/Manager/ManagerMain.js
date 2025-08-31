935d5f97511f00f8de4b7f3c261e544b5fe54423
jyd0328 <jydjyd0328@gmail.com>
2025-08-31 15:02:56 +0900

    fix:logo

--- frontend/src/pages/Manager/ManagerMain.js
+++ frontend/src/pages/Manager/ManagerMain.js
@@ -30,16 +30,7 @@ export const ManagerMain = () => {
         <div className={styles.topBar}>
           <HomeButton to="/manager" />
           <div className={styles.logoPanel}>
-            <img
-              src="/icons/YuSeong_Icon.png"
-              alt="유학대학로고"
-              className={styles.yuseongIcon}
-            />
-            <img
-              src="/icons/Sowlmate_Icon.png"
-              alt="솦꿉친구로고"
-              className={styles.sowlmateIcon}
-            />
+            <img src="/icons/logo.svg" alt="로고" className={styles.logo} />
           </div>
         </div>
 
