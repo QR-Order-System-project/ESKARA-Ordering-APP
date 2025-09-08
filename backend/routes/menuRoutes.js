57ef20af2e11c3219bcf00e11e04cdba92422d84
k-heeseung <rnjs050902@g.skku.edu>
2025-09-08 20:48:13 +0900

    feat(be): fill menuqueue

--- backend/routes/menuRoutes.js
+++ backend/routes/menuRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { finishMenu, showMenuQueue } = require('../controllers/orderController');
+const { finishMenu, showMenuQueue, fillMenuQueue } = require('../controllers/orderController');
 
 const router = express.Router();
 
@@ -8,5 +8,9 @@ router.post('/finish', finishMenu);
 
 // 메뉴 큐 보여주기
 router.get('/showMenuQueue', showMenuQueue);
+
+// 메뉴 큐 채우기 (초기화용, 필요시 사용)
+router.get('/fillMenuQueue', fillMenuQueue);
+
 
 module.exports = router;
